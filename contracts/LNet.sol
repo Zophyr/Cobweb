3ed7f4564dcc5ee764fffa2b67ff24d86c8f800b
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-12-26 11:35:58 +0800

    Build LNet demo

--- contracts/LNet.sol
+++ contracts/LNet.sol
@@ -1,10 +1,27 @@
 pragma solidity ^0.5.0;
 
 contract LNet {
+    // model a bill
+    struct Bill {
+        uint id;
+        uint money;
+        address giver;
+        address asker;
+    }
 
-    string public dog;
+    // store bills
+    // fetch bills
+    mapping(uint => Bill) public bills;
+
+    uint public billCount;
 
     constructor () public {
-        dog = "dog 1";
+        _createBill(100, 0xEf07122bbf3A2DCcc4085E83650bA781F4Db4AE7, 0x62a9367Ae2ff42B7b58c9DF7a7572c09816F4196);
+        _createBill(100, 0xa3E60385738e626BD09F79FAd5151D41dB8E6034, 0x5c922c7aF44FA622AD759B2829422CAa4a6f6677);
+    }
+
+    function _createBill (uint _money, address _giver, address _asker) private {
+        billCount ++;
+        bills[billCount] = Bill(billCount, _money, _giver, _asker);
     }
 }
