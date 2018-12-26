1e8a4f507407eb893607a6d11abc5a9848c601a0
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-12-26 12:41:48 +0800

    Update lnet.js

--- test/lnet.js
+++ test/lnet.js
@@ -1,6 +1,7 @@
 var LNet = artifacts.require("./LNet.sol");
 
 contract("LNet", function() {
+    var lnetInstance;
     
     it("initializes with two bills", function() {
         return LNet.deployed().then(function(i) {
@@ -9,4 +10,22 @@ contract("LNet", function() {
             assert.equal(count, 2);
         });
     });
+
+    it("it initializes the bills with the correct values", function() {
+        return LNet.deployed().then(function(i) {
+            lnetInstance = i;
+            return lnetInstance.bills(1);
+        }).then(function(bill) {
+            assert.equal(bill[0], 1, "contains the correct id");
+            assert.equal(bill[1], 100, "contains the correct money");
+            assert.equal(bill[2], 0xEf07122bbf3A2DCcc4085E83650bA781F4Db4AE7, "contains the correct giver");
+            assert.equal(bill[3], 0x62a9367Ae2ff42B7b58c9DF7a7572c09816F4196, "contains the correct asker");
+            return lnetInstance.bills(2);
+        }).then(function(bill) {
+            assert.equal(bill[0], 2, "contains the correct id");
+            assert.equal(bill[1], 500, "contains the correct money");
+            assert.equal(bill[2], 0xa3E60385738e626BD09F79FAd5151D41dB8E6034, "contains the correct giver");
+            assert.equal(bill[3], 0x5c922c7aF44FA622AD759B2829422CAa4a6f6677, "contains the correct asker");
+        })
+    })
 });
