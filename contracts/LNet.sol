49c94463b1264bbd0cfa5a8de7fa2ffa01d55967
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-12-26 12:41:40 +0800

    RESET Migrations

--- contracts/LNet.sol
+++ contracts/LNet.sol
@@ -17,7 +17,7 @@ contract LNet {
 
     constructor () public {
         _createBill(100, 0xEf07122bbf3A2DCcc4085E83650bA781F4Db4AE7, 0x62a9367Ae2ff42B7b58c9DF7a7572c09816F4196);
-        _createBill(100, 0xa3E60385738e626BD09F79FAd5151D41dB8E6034, 0x5c922c7aF44FA622AD759B2829422CAa4a6f6677);
+        _createBill(500, 0xa3E60385738e626BD09F79FAd5151D41dB8E6034, 0x5c922c7aF44FA622AD759B2829422CAa4a6f6677);
     }
 
     function _createBill (uint _money, address _giver, address _asker) private {
