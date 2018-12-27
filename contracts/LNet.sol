d69dc7a5da640c40d32ee2cb1dd382f81d0248e3
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-12-27 17:10:14 +0800

    「UPDATE」add createBill function
    添加创建合约函数

--- contracts/LNet.sol
+++ contracts/LNet.sol
@@ -1,10 +1,31 @@
 pragma solidity ^0.5.0;
 
 contract LNet {
+    // model a bill
     struct Bill {
         uint id;
         uint money;
         address giver;
         address asker;
+    }
+
+    // store bills
+    // fetch bills
+    mapping(uint => Bill) public bills;
+
+    // bill id
+    uint public billCount;
+
+    // everyone use to new a bill
+    function createBill (uint _money, address _giver, address _asker) public payable {
+        require(msg.value == _money, "Giver money whether enought");
+        require(msg.sender == _giver, "address right?");
+        _createBill(_money, _giver, _asker);
+    }
+
+    // to new a bill
+    function _createBill (uint _money, address _giver, address _asker) private {
+        billCount ++;
+        bills[billCount] = Bill(billCount, _money, _giver, _asker);
     }
 }
