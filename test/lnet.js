4ee7240af1db886594e51ca0a4a2ccd73d8baff0
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-12-27 17:22:01 +0800

    「UPDATE」add test for createBill function
    增加测试创建合约函数的函数

--- test/lnet.js
+++ test/lnet.js
@@ -3,6 +3,13 @@ var LNet = artifacts.require("./LNet.sol");
 contract("LNet", function () {
     var addressList;
 
+    var lnetInstance;
+
+    // the input must is stirng type.
+    function toETH(input) {
+        return web3.utils.toWei(input, 'ether');
+    }
+
     it("Connect the blockchain and find the address.", function () {
         return web3.eth.getAccounts().then(function (i) {
             addressList = i;
@@ -10,5 +17,31 @@ contract("LNet", function () {
         }).then(function (ads) {
             assert.equal(ads.length, 20, "Found 20 addresses.");
         })
+    });
+
+    it("Create two bills.", function () {
+        return LNet.deployed().then(function (i) {
+            lnetInstance = i;
+            lnetInstance.createBill(toETH('1'), addressList[1], addressList[2], {
+                from: addressList[1],
+                value: toETH('1')
+            });
+            lnetInstance.createBill(toETH('10'), addressList[3], addressList[4], {
+                from: addressList[3],
+                value: toETH('10')
+            });
+            return lnetInstance.bills(1);
+        }).then(function (bill) {
+            assert.equal(bill.id, 1, "contains the correct id");
+            assert.equal(bill.money, toETH('1'), "contains the correct money");
+            assert.equal(bill.giver, addressList[1], "contains the correct giver");
+            assert.equal(bill.asker, addressList[2], "contains the correct asker");
+            return lnetInstance.bills(2);
+        }).then(function (bill) {
+            assert.equal(bill.id, 2, "contains the correct id");
+            assert.equal(bill.money, toETH('10'), "contains the correct money");
+            assert.equal(bill.giver, addressList[3], "contains the correct giver");
+            assert.equal(bill.asker, addressList[4], "contains the correct asker");
+        });
     });
 });
