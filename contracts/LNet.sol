078654644ef643bfdc96cc2cdfb8ffa3d3beb51a
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-12-27 18:51:41 +0800

    「UPDATE」add takemoney function
    添加借钱的人的取钱函数

--- contracts/LNet.sol
+++ contracts/LNet.sol
@@ -18,9 +18,16 @@ contract LNet {
 
     // everyone use to new a bill
     function createBill (uint _money, address _giver, address _asker) public payable {
-        require(msg.value == _money, "Giver money whether enought");
-        require(msg.sender == _giver, "address right?");
+        require(msg.value == _money, "The money is the same as the money previously registered. ");
+        require(msg.sender == _giver, "The request was sent by giver.");
         _createBill(_money, _giver, _asker);
+    }
+
+    // asker can use this methon to get money
+    function takeMoney (uint _id, uint _money) public {
+        require(_money == bills[_id].money, "The money is the same as the money previously registered. ");
+        require(msg.sender == bills[_id].asker, "The request was sent by asker.");
+        msg.sender.transfer(_money);
     }
 
     // to new a bill
