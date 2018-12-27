4a9cddad9a3f7ad0696353a3c3b0caffa57bd9e8
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2018-12-27 19:33:02 +0800

    「UPDATE」add status judgment
    添加状态判断，防止双花

--- contracts/LNet.sol
+++ contracts/LNet.sol
@@ -7,6 +7,9 @@ contract LNet {
         uint money;
         address giver;
         address asker;
+        bool isTake;
+        bool isBack;
+        bool isDone;
     }
 
     // store bills
@@ -27,6 +30,8 @@ contract LNet {
     function takeMoney (uint _id, uint _money) public {
         require(_money == bills[_id].money, "The money is the same as the money previously registered. ");
         require(msg.sender == bills[_id].asker, "The request was sent by asker.");
+        require(!bills[_id].isTake, "Asker never take money.");
+        bills[_id].isTake = true;
         msg.sender.transfer(_money);
     }
 
@@ -34,18 +39,22 @@ contract LNet {
     function backMoney (uint _id) public payable {
         require(msg.value == bills[_id].money, "The money is the same as the money previously registered. ");
         require(msg.sender == bills[_id].asker, "The request was sent by asker.");
+        bills[_id].isBack = true;
     }
 
     // giver can use this methon to callback money
     function healMoney (uint _id, uint _money) public {
         require(_money == bills[_id].money, "The money is the same as the money previously registered. ");
         require(msg.sender == bills[_id].giver, "The request was sent by giver.");
+        require(bills[_id].isBack, "Asker had return money.");
+        require(!bills[_id].isDone, "Giver never take money.");
+        bills[_id].isDone = true;
         msg.sender.transfer(_money);
     }
 
     // to new a bill
     function _createBill (uint _money, address _giver, address _asker) private {
         billCount ++;
-        bills[billCount] = Bill(billCount, _money, _giver, _asker);
+        bills[billCount] = Bill(billCount, _money, _giver, _asker, false, false, false);
     }
 }
